68f06f2d461dd2af8e629670f9c87bbc89384ef0
itsarunkumar <003arunkumark@gmail.com>
2023-08-06 15:21:40 +0530

    changes

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,11 +6,10 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function formatDate(dateString: string): string {
-  const options = {
+  return new Date(dateString).toLocaleString("en-US", {
     month: "short",
     day: "2-digit",
     hour: "2-digit",
     hour12: false,
-  };
-  return new Date(dateString).toLocaleString("en-US", options);
+  });
 }
